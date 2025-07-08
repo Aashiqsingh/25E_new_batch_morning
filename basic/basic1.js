5fee340f4c2cbebdbe0ca119fe65ba3a399307b4
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-08 10:57:58 +0530

    basic , intro

---
+++ basic/basic1.js
@@ -0,0 +1,38 @@
+// console.log("Good mornning");
+// console.info("Information function...");
+// console.warn("Warning function...");
+// console.error("Error function...");
+
+
+// Datatype : number , string , boolean , undefined , object
+
+
+a = 10;
+console.log("Value of a = ",a);
+console.log("typeof a = ",typeof(a));
+
+
+b = 12.55;
+console.log("Value of b = ",b);
+console.log("typeof b = ",typeof(b));
+
+
+c = "shree";
+console.log("Value of c = ",c);
+console.log("typeof c = ",typeof(c));
+
+isAvailable = true;
+console.log("Value of isAvailable = ",isAvailable);
+console.log("typeof isAvailable = ",typeof(isAvailable));
+
+
+var d;
+console.log("Value of d = ",d);
+console.log("typeof d = ",typeof(d));
+
+var e = null;
+console.log("Value of e = ",e);
+console.log("typeof e = ",typeof(e));
+
+
+
---
+++ operator/operator.js
@@ -0,0 +1,34 @@
+var a = "10";
+var b = "10";
+
+
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Addition = ",a+b);
+// console.log("Addition = "+(a+b));
+
+
+
+// console.log(7/2);
+
+// console.log(7%2);
+
+// console.log(4 ** 3);
+// console.log(3 ** 4);
+
+
+
+
