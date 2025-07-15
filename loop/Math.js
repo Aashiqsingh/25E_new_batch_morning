f26292a91f9935c6402b0b32f2bfc3aaf7510697
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-15 10:47:58 +0530

    loop , Math

---
+++ loop/Math.js
@@ -0,0 +1,24 @@
+// console.log(13/2);
+
+
+// console.log(Math.floor(13.99));
+// console.log(Math.ceil(13.01));
+// console.log(Math.round(13.49));
+
+// console.log(Math.min(13,34,45,56,67,7,89,90));
+// console.log(Math.max(13,34,45,56,100,7,89,90));
+
+
+// console.log(Math.pow(2,4));
+
+
+// console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.floor(100000 + Math.random() * 900000));
+
+
+
+
+
+
+
---
+++ loop/loop.js
@@ -0,0 +1,60 @@
+// for(let i=1;i<=10;i++){
+//     console.log("hello world..",i);
+// }
+
+
+
+
+
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i= 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+
+// reverse number 
+
+// let num = 3457;
+
+// let rev = 0;
+
+// for(;num>0;){
+//     let rem = num % 10;
+//     rev = rev * 10 + rem;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(rev);
+
+
+
+
+
+
+
+
+
+// let rev = 0;
+
+// while(num!=0){
+//     let rem = num % 10;
+//     rev = rev * 10 + rem;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(rev);
+
+
+// let num = 13;
+
+// for(let i=1;i<=10;i++){
+//     console.log(num + "*" + i + "="+num*i );
+    
+// }
