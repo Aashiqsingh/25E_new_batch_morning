0ae1be9d38318534a797d9adfee9ec89c64f32e8
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-30 11:00:10 +0530

    array method

---
+++ Array/array1.js
@@ -0,0 +1,79 @@
+// array : hybrid
+
+// var arr = [12,45.66,true,"harshil"];
+// console.log(arr);
+// console.log(arr.length);
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+// var users = ["harshil","aashi","ankit","rahul","vivek","prashant","nitin","anil","anuj"];
+
+// users.push("bhuwan")
+// users.push("bhuwan","harshil")
+
+// console.log(users); 
+
+// users.pop()
+
+// console.log(users);
+
+
+// users.unshift("bhuwan")
+// console.log(users);
+
+// users.shift()
+
+// console.log(users);
+
+// let x = users.indexOf("vivek")
+// console.log(x);
+
+// let x = users.includes("vivek")
+// console.log(x);
+
+
+// let x = users.reverse()
+// console.log(x);
+
+
+// let x = users.slice(1,4)
+// console.log(x);
+
+// let x = users.sort()
+// console.log(x);
+
+var users = ["harshil","aashi","ankit","rahul","vivek","prashant","nitin","anil","anuj"];
+
+
+// users.splice(3,0,"bhuwan")
+// users.splice(3,3,"bhuwan")
+// users.splice(4,2)
+
+
+// console.log(users);
+
+
+
+// let x = users.join(" ")
+// let x = users.join("$")
+
+// console.log(x);
+
+
+
+
+// console.log(users.length);
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].toUpperCase());    
+// }
+
+
+str = "aashiqsingh";
+
+
+let x =  str.split("").reverse().join("")
+console.log(x);
